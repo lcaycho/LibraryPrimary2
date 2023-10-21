8da699b25a0a42b4d77b89cd6a2ae7421e029a49
Leonardo Caycho <leonardo_caycho@usmp.pe>
2023-10-21 17:17:39 -0500

    Index y nosotros

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,11 @@ public class HomeController : Controller
         return View();
     }
 
+     public IActionResult Nosotros()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
